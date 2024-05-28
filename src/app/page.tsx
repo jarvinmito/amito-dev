d0c5249b0d23722788c6e2eb70c59b80fef0fc3f
Arvin <mito.jan.arvin@gmail.com>
2024-05-28 13:12:32 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@mantine/core";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Hello world</h1>
+    </main>
+  );
+}
